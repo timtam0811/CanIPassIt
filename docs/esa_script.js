0b7890c5f73cfe7f1841ef6fe7a1e1ca9064879f
timtam0811 <akg.fob.811@gmail.com>
2019-08-18 22:14:55 +0900

    add js, result, style mod

---
+++ docs/esa_script.js
@@ -0,0 +1,26 @@
+function submit(){
+    document.getElementById("form-wrapper").classList.add("result");
+    $("body").scrollTop(0);
+    
+    var query=document.getElementById("select-question");
+    var answer=document.getElementById("answer-text");
+    var queryResult=document.getElementById("question-result");
+    var answerResult=document.getElementById("answer-result");
+
+    var qNode=document.createTextNode(query.value);
+    var aNode=document.createTextNode(answer.value);
+    
+    queryResult.removeChild(queryResult.firstChild);
+    answerResult.removeChild(answerResult.firstChild);
+
+    queryResult.appendChild(qNode);
+    answerResult.appendChild(aNode);
+}
+
+function return_answer(){
+    document.getElementById("form-wrapper").classList.remove("result");
+    $("body").scrollTop(0);
+}
+
+document.getElementById("submit-button").addEventListener("click",submit);
+document.getElementById("return-button").addEventListener("click",return_answer);
